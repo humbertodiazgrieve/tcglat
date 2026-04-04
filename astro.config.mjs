e95fe54187ec2aeecb159ebfd6553f756f228dc2
Humberto Diaz Grieve <129482732+humbertodiazgrieve@users.noreply.github.com>
2026-04-04 11:24:07 -0500

    deploy error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://tcglat.com',
   output: 'hybrid',
-  adapter: node({ mode: 'standalone' }),
-  server: {
-    port: 4322
-  },
-  build: {
-    format: 'file'
-  },
+  adapter: cloudflare(),
   integrations: [
     tailwind(), 
     react()
